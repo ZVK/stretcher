fa3b7fcc1caeab6c0a4c496c7122df55029607c6
ZVK <drawordoodle@gmail.com>
2016-02-12 00:39:35 -0500

    Designed a UI
    implemented bootstrap-slider, wave-surfer, audio transport buttons, and
    custom font.

--- test.js
+++ test.js
@@ -24,14 +24,22 @@ var state = {
 var wavesurfer = WaveSurfer.create({
     audioContext: context,
     container: '#wave',
-    waveColor: 'violet',
-    progressColor: 'purple',
+    waveColor: 'blue',
+    progressColor: 'orange',
     audioRate: 1,
     normalize: true
 });
 wavesurfer.on('play', function(e){
     console.log("playing")
-})
+});
+wavesurfer.on('ready', function () {
+    var timeline = Object.create(WaveSurfer.Timeline);
+
+    timeline.init({
+        wavesurfer: wavesurfer,
+        container: "#wave-timeline"
+    });
+});
 
 wavesurfer.on('loading', function(percent, request) {
     console.log(percent);
@@ -57,10 +65,21 @@ wavesurfer.on('loading', function(percent, request) {
     }
 });
 
+wavesurfer.on('finish', function () {
+    console.log('Finished playing');
+});
+
 function set_tempo(speed){
     console.log("setting speed to "+speed);
     st.tempo = speed;
     wavesurfer.setPlaybackRate(speed);
+}
+
+function set_pitch(semitones){
+    //calculate factor to multiply frequency by
+    var frequency_factor = Math.pow(2, (semitones/12));
+    console.log("setting pitch to "+frequency_factor+"% ("+semitones+" semitones)");
+    st.pitch = frequency_factor;
 }
 
 //function to set a batch of attributes for an HTML element and object pair
@@ -74,22 +93,41 @@ function init(){
     wavesurfer.load('./Lightning Bolt - 13 Monsters.mp3');
     //make the tempo slider we prepared above
     // Instantiate a tempo slider
-    var slider = document.createElement("INPUT");
-    //store preset attributes
-    var slider_attributes = {
-        // initial options object
-        id: "tempo_slider",
-        type: "range",
-        min: 0.35,
-        max: 2.0,
-        step: 0.05,
-        value: 1.0,
-        handle: "triangle",
-        onchange: "set_tempo(this.value);"
+    var tempo = {
+        element: document.createElement("INPUT"),
+        //store preset attributes
+        styles: {
+            // initial options object
+            id: "tempo_slider",
+            type: "range",
+            min: 0.35,
+            max: 2.0,
+            step: 0.05,
+            value: 1.0,
+            onchange: "set_tempo(this.value);"
+        }
+    }, 
+    pitch = {
+        element: document.createElement("INPUT"),
+        //store preset attributes
+        styles: {
+            // initial options object
+            id: "pitch_slider",
+            type: "range",
+            min: -24,
+            max: 24,
+            step: 1,
+            value: 1,
+            onchange: "set_pitch(this.value);"
+        }
     };
-    set_attributes(slider, slider_attributes);
+    //build sliders
+    set_attributes(tempo.element, tempo.styles);
+    set_attributes(pitch.element, pitch.styles)
     //add it to in at the bottom of our html body
-    document.body.appendChild(slider)
+    document.getElementById("tempo").appendChild(tempo.element);
+    document.getElementById("pitch").appendChild(pitch.element);
+
 }
 
 function play() {
